2352697793959b605f397cae158c044f7f4df91a
Leo Dion <leogdion@brightdigit.com>
2024-07-20 21:02:44 -0400

    working on running commands

--- Sources/Swoop/Swoop.swift
+++ Sources/Swoop/Swoop.swift
@@ -4,8 +4,36 @@ import ArgumentParser
 
 import Foundation
 
+struct ShellOutput {
+  let output: String
+  let error : String
+}
+
+struct TerminationError : Error {
+  private init(status: Int, reason: Int, output: ShellOutput) {
+    self.status = status
+    self.reason = reason
+    self.output = output
+  }
+  
+  let status : Int
+  let reason : Int
+  let output: ShellOutput
+  
+  init?(status: Int, reason: Int, output: ShellOutput, accceptableStatus: @Sendable @escaping (Int) -> Bool) {
+    let statusFailed = accceptableStatus(status)
+    
+    guard !statusFailed || reason != 1 else {
+      return nil
+    }
+    
+    self.init(status: status, reason: reason, output: output)
+  }
+}
+
+
 // Function to run a shell command and capture its output
-func runShellCommand(_ command: String, arguments: [String] = []) async throws -> (output: String?, error: String?) {
+  func runShellCommand(_ command: String, arguments: [String] = [], accceptableStatus: @Sendable @escaping (Int) -> Bool = {$0 == 0}) async throws -> ShellOutput {
     let process = Process()
     process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
     process.arguments = [command] + arguments
@@ -14,29 +42,44 @@ func runShellCommand(_ command: String, arguments: [String] = []) async throws -
     let errorPipe = Pipe()
     process.standardOutput = outputPipe
     process.standardError = errorPipe
-  
-  return try await withCheckedThrowingContinuation { continuation in
-    do {
+    
+    return try await withCheckedThrowingContinuation { continuation in
+      do {
         try process.run()
-    } catch {
-      continuation.resume(throwing: error)
-//        print("Failed to run command: \(error)")
-//        return (nil, error.localizedDescription)
+      } catch {
+        continuation.resume(throwing: error)
+        //        print("Failed to run command: \(error)")
+        //        return (nil, error.localizedDescription)
+      }
+      
+      process.waitUntilExit()
+      
+      print(process.terminationReason.rawValue)
+      print(process.terminationStatus)
+      
+      let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+      let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+      
+      let outputString = String(decoding: outputData, as: UTF8.self)
+      let errorString = String(decoding: errorData, as: UTF8.self)
+      
+      let output =  ShellOutput(output: outputString, error: errorString)
+      
+      if let error = TerminationError(
+        status: Int(process.terminationStatus),
+        reason: process.terminationReason.rawValue,
+        output: output,
+        accceptableStatus: accceptableStatus
+      ) {
+        continuation.resume(throwing: error)
+      } else {
+        continuation.resume(returning: output)
+      }
     }
-    
-    process.waitUntilExit()
-    
-    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-    let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-    
-    let outputString = String(data: outputData, encoding: .utf8)
-    let errorString = String(data: errorData, encoding: .utf8)
-    
-    continuation.resume(returning: (outputString, errorString))
   }
   
    
-}
+
 
 // Example usage: Running 'brew list' to list installed Homebrew packages
 //let result = runShellCommand("brew", arguments: ["list"])
@@ -46,12 +89,22 @@ struct Swoop : AsyncParsableCommand {
   
   
   mutating func run() async throws {
+    do {
+      try await runShellCommand("asdf")
+    } catch {
+      dump(error)
+    }
+  }
+}
+
+struct Brew {
+  func run() async throws {
     let result = try await runShellCommand("brew", arguments: ["list", "-1", "--full-name"])
-    if let lines =  result.output?.components(separatedBy: "\n") {
+    let lines =  result.output.components(separatedBy: "\n")
       
       print(lines.count)
       print(lines)
-    }
+    
   }
 }
 
